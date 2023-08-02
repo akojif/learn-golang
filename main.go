df7dde0629c3fc4197cc3c3c308421199e772623
Francis Akoji <akfrendo@gmail.com>
2023-08-02 15:46:08 +0100

    random stuff

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	fmt.Printf("The type of 1000 is %v \n", reflect.TypeOf(1000))
-	fmt.Printf("The data type of Francis is %v \n", reflect.TypeOf("Francis"))
-	fmt.Printf("The data type of 43.67 is %v \n", reflect.TypeOf(43.67))
-
+	var a, b string = "Fran", "cis"
+	fmt.Println(a + b)
 }
