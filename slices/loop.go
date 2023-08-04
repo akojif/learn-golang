bc743fd89de67ac5722077d55cf4fa6db6538e69
Francis Akoji <akfrendo@gmail.com>
2023-08-04 15:54:10 +0100

    more stuff on slices

--- slices/loop.go
+++ slices/loop.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	slice := []int{10, 20, 30, 40, 50}
 
-	for index, element := range slice { // can be either value or element
-		fmt.Println(index, "=>", element)
+	for index, element := range slice { // for use cases where you don't want to return the index, you can just represent the index as _
+		fmt.Println(index, "=>", element) // can be either value or element
 	}
 }
